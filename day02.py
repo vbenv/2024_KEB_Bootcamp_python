367e0a8d1bba3e717d743aac6d36d94d12321d00
vben <tjdcksl20@gmail.com>
2024-01-15 13:16:43 +0900

    wow

--- day02.py
+++ day02.py
@@ -55,4 +55,11 @@ print(type(cash)) #int
 
 base_number = int(input('Input base number : '))
 exponent_number = int(input('Input base number : '))
-print(f'밑은 {base_number}, 지수는{exponent_number}, 결과 값은 {base_number**exponent_number}')
+# f-string
+#print(f'밑은 {base_number}, 지수는{exponent_number}, 결과 값은 {(base_number**exponent_number)}')
+#print(f'밑은 {base_number}, 지수는{exponent_number}, 결과 값은 {pow(base_number, exponent_number)}')
+
+#f-function
+print('밑은 {0}, 지수는 {1}, 결과 값은 {2}'.format(base_number, exponent_number, pow(base_number, exponent_number)))
+
+
