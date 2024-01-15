d9903187aec04f338194273a8c1d13160393e158
vben <tjdcksl20@gmail.com>
2024-01-15 11:22:54 +0900

    v0.2 python day02

---
+++ day002.py
@@ -0,0 +1,23 @@
+univ = "Inha university"
+print(univ)
+print(univ[5])
+# univ[5] = 'U' // imutable
+# print(univ)
+subjects = ['python', 'c++', 'linux', 'data structure', 'database']
+print(subjects)
+print(subjects[3])
+subjects[3] = 'data structrue & algorithm' #mutable
+print(subjects)
+
+
+# print(0.1)
+# print(1e-1)
+# print(0.01)
+# print(1e-2)
+# print(3.14)
+# print(314e-2)
+# print(314.1592)
+# print(0.3141592e3)
+# print(21000)
+# print(21_000)
+
