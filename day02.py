8f785cc1c04441e822878d770c7612bbcb2472c7
vben <tjdcksl20@gmail.com>
2024-01-15 15:54:01 +0900

    v1.4 python day02

---
+++ day02.py
@@ -0,0 +1,8 @@
+letter = input('Input alphabet letter : ')
+#vowels = {'a', 'e', 'i', 'o', 'u'}  # set
+vowels = "aeiuo"  # str
+print(type(vowels))
+if letter in vowels:  # in
+    print(f'{letter} is a vowel~')
+else:
+    print(f'{letter} is a consonant!')
