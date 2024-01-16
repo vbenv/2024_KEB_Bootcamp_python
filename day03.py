71930c544c206a083a40b33dd49ef0dcf6a0595c
vben <tjdcksl20@gmail.com>
2024-01-16 14:11:22 +0900

    prime number_0.2

--- day03.py
+++ day03.py
@@ -7,7 +7,10 @@ i = 2
 while i < number:
     if number % i == 0:
         cnt += 1
+        break   #break 하나로 반복 횟수를 줄일 수 있음.
+    print(i, end=' ') #몇 번 도는지 보기 위한 확인용 코드
     i += 1
+
 if cnt == 0:
     print(f'{number} is prime number')
 else:
