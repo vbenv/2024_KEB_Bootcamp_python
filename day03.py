095cd8ef18ec18737621504941401a936935f2f4
vben <tjdcksl20@gmail.com>
2024-01-16 11:45:22 +0900

    v1.7

--- day03.py
+++ day03.py
@@ -1,39 +1,15 @@
-# split 해보기 -> 구분자를 기준으로 하나의 문자열을 여러 개의 리스트로 나눔
-course = "2024 KEB Bootcamp"
-# print(course)
-# list_course = course.split('B')
-# print(list_course)
-#
-# # split으로 응용해보기
-# #numbers = input("FirstNumber SecondNumber : ").split()
-# #print(numbers[0] + numbers[1])     #concatenation : 연속
-# #print(int(numbers[0]) + int(numbers[1]))     #arithmetic operation : 산술 연산
-#
-# #join : list -> str.
-# subjects = ["python", "c++", "database"]
-# subjects_string = " / ".join(subjects)
-# print(subjects_string)
-# print(",".join('abbbcdd'))
-#
-# #replace('원래 문자', '바꿀 문자', 원하는 바꿀 횟수)
-# print(course.replace('KEB', 'Inha'))
-# print(course)
-# course = course.replace('KEB', 'Inha')  # immutable의 원본을 바꾸기 위해서는 재할당을 해줘야 함+ tuple
-# print(course)
-#
-# #원하는 횟수만큼만 바꿀 수 있음.
-courses = "* KEB .KEB #KEB !2024 Inha University...*!"
-# #print(courses.replace('KEB', 'Inha', 2))
+#예외 발생 시 대처
+subjects = "python c++ database linux"
+subject = input("수강신청과목 입력 : ")
 
+try:
+    print(f"해당 과목이 존재합니다. 위치는 {subjects.index(subject)}번 인덱스입니다.")
+except ValueError:
+    print('해당 과목이 존재하지 않습니다.')
 
-# strip("없애줄 문자들") 양쪽 끝 군더더기들을 지워주는 함수.
-print(courses.strip())
-print(courses.strip("!#.*"))
-
-#검색 find, rfind, index -> 위치를 알려줌
-print(courses.find("KEB"))
-print(courses.rfind("KEB"))
-print(courses.index("KEB"))
-
-print(courses.find("yahoo"))    # 못 찾으면 '-1'를 출력.
-print(courses.index("yahoo"))   # ValueError: substring not found
+print(subjects[subjects.find(subject)])
+#Q.subjects에 없는 과목 입력하면 에러가 뜬다는 데 안 뜸
+if subjects.find(subject) != -1:
+    print("있습니다")
+else:
+    print("없습니다.")
