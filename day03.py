d9cf84fc9fb7981113ed1600369d75a1083636b9
vben <tjdcksl20@gmail.com>
2024-01-16 14:40:53 +0900

    prime number_0.4

--- day03.py
+++ day03.py
@@ -2,17 +2,19 @@
 # 6장 반복문
 #prime number
 number = int(input("input number : "))
-is_prime = True     # int -> bool
-i = 2
-while i < number:
-    if number % i == 0:
-        is_prime = False    # remove +
-        break   #break 하나로 반복 횟수를 줄일 수 있음.
-    #print(i, end=' ') #몇 번 도는지 보기 위한 확인용 코드
-    i += 1
-
-#if cnt ==0:
-if is_prime:    #remove ==
-    print(f'{number} is prime number')
-else:
+is_prime = True
+if number < 2:
     print(f'{number} is NOT prime number!')
+else:
+    i = 2
+    while i < number:
+        if number % i == 0:
+            is_prime = False
+            break
+        i += 1
+
+
+    if is_prime:
+        print(f'{number} is prime number')
+    else:
+        print(f'{number} is NOT prime number!')
