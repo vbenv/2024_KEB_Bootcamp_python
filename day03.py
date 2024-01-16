02de5b579c5b2b4495b3b46453117aef64c87b5a
vben <tjdcksl20@gmail.com>
2024-01-16 14:03:46 +0900

    prime number

--- day03.py
+++ day03.py
@@ -1,8 +1,14 @@
-# formating
-print('%s' % 42) # old version
 
-print('%e' % 0.703)
-
-#new version
-subjects = {'python' : 'kim', 'c++' : 'sung', 'data structure' : 'kim', 'data base' : 'kang'}
-print("{0[c++]} {0[data structure]}".format(subjects))  # 숫자는 인덱스 번호를 의미. 그 안에 있는 것은 키 값을 불러옴. 그 키 값에 해당하는 value값 불러옴
+# 6장 반복문
+#prime number
+number = int(input("input number : "))
+cnt = 0
+i = 1
+while i <= number:
+    if number % i == 0:
+        cnt += 1
+    i += 1
+if cnt == 2:
+    print(f'{number} is prime number')
+else:
+    print(f'{number} is NOT prime number!')
