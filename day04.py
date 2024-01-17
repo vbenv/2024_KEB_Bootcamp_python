9e6a6d3108bbbf20d17918d30c4c472d804fb76e
vben <tjdcksl20@gmail.com>
2024-01-17 14:56:17 +0900

    2.4 day04

--- day04.py
+++ day04.py
@@ -1,18 +1,17 @@
-#8chapter "dict, set"
-sugang = dict(python = "kim", cpp = "sung", db = "kang")
-# print(sugang)
-# sugang["datastructure"] = 'kim' # add
-# print(sugang)
-# sugang["datastructure"] = 'park'    # update
-# print(sugang)
-# print(sugang['db'])     #[key]
-# print(sugang.get('db')) #get()
-# print(sugang.get('ooo', 'not exist')) #get (키, none 출력어 설정)
-for subject, professor in sugang.items():
-    print(f'{subject} 과목 담당교수는 {professor}입니다.')
+drinks_foods = {"위스키" : "초콜릿", "와인" : "치즈", "소주" : "삼겹살", "고량주" : "양꼬치"}
 
-for s in sugang.items():    #튜플 형식으로 받기.
-    print(s)
-print()
-for v in sugang.values():
-    print(v)
+#drink = input(drinks_foods.keys())
+drinks_foods_keys = list(drinks_foods)
+while True:
+    menu = input(f'다음 술 중에 고르세요. \n1) {drinks_foods_keys[0]},  2) {drinks_foods_keys[1]},  3) {drinks_foods_keys[2]},  4) {drinks_foods_keys[3]},   5) 먹고 싶은 게 없네요.')
+    if menu == '1': #숫자로 쓰면 안됨
+        print(f'{drinks_foods_keys[0]}에 어울리는 안주는 {drinks_foods[drinks_foods_keys[0]]} 입니다.')
+    elif menu == '2':
+        print(f'{drinks_foods_keys[1]}에 어울리는 안주는 {drinks_foods[drinks_foods_keys[1]]} 입니다.')
+    elif menu == '3':
+        print(f'{drinks_foods_keys[3]}에 어울리는 안주는 {drinks_foods[drinks_foods_keys[2]]} 입니다.')
+    elif menu == '4':
+        print(f'{drinks_foods_keys[2]}에 어울리는 안주는 {drinks_foods[drinks_foods_keys[3]]} 입니다.')
+    elif menu == '5':
+        print(f'다음에 또 오세요')
+        break
