b5ba5174ffc81cf8ec849385dce699cb7b6459bb
vben <tjdcksl20@gmail.com>
2024-01-17 09:23:22 +0900

    v0.1python day03_assignment

--- day04.py
+++ day04.py
@@ -1,24 +1,20 @@
-choice = int(input("문제를 골라주세요. \n 1) 화씨를 섭씨로 변환 2) 화씨 또는 섭씨 변환 3)소수 판정 4)구간 소수 5)QUIT\n : "))
+# Assignment (add prime series program)
+
+choice = int(input("문제를 골라주세요. \n 1) Fahrenheit -> Celsius 2) Celsius -> Fahrenheit 3) Prime1 4) Prime 2 5)QUIT\n : "))
 # 1번 : 화씨 -> 섭씨 변환
 if choice == 1:
     fahrenheit = float(input('Input Fahrenheit : '))
     print(f'Fahrenheit : {fahrenheit}F, Celsius : {((fahrenheit - 32.0) * 5.0 / 9.0):.4f}C')
 
 # 2번 : 화씨 또는 섭씨 변환
-if choice == 2:
-    menu = input("1) Fahrenheit -> Celsius   2) Celsius -> Fahrenheit   3) Quit program : ")
+elif choice == 2:
+    celsius = float(input('Input Celsius : '))
+    print(f'Celsius : {celsius}C, Fahrenheit : {((celsius*9.0/5.0) + 32.0):.4f}F')
 
-    if menu == '1':
-        fahrenheit = float(input('Input Fahrenheit : '))
-        print(f'Fahrenheit : {fahrenheit}F, Celsius : {((fahrenheit - 32.0) * 5.0 / 9.0):.4f}C')
-    elif menu == '2':
-        celsius = float(input('Input Celsius : '))
-        print(f'Celsius : {celsius}C, Fahrenheit : {((celsius * 9.0 / 5.0) + 32.0):.4f}F')
-    else:
-        print('Terminate Program.')
+
 
 # 3번 : 소수 판정
-if choice == 3:
+elif choice == 3:
     number = int(input("input number : "))
     is_prime = True
     if number < 2:
@@ -34,7 +30,7 @@ if choice == 3:
             print(f'{number} is NOT prime number!')
 
 # 4번 : 구간소수
-if choice == 4:
+elif choice == 4:
     numbers = input("input first second number : ").split()
     n1 = int(numbers[0])
     n2 = int(numbers[1])
@@ -53,6 +49,6 @@ if choice == 4:
                     is_prime = False
                     break
             if is_prime: print(number, end=' ')
-
+    print()
 if choice == 5:
     print("Terminate Program")
