59081c04b68585a0910a276e756b82f696699902
vben <tjdcksl20@gmail.com>
2024-01-17 09:47:11 +0900

    v0.2 python day03_assignment

--- day04.py
+++ day04.py
@@ -1,5 +1,11 @@
 # Assignment (add prime series program)
 
+#현재 이 코드의 문제점
+#1. 효율의 문제(3,4) => 2로 나눴을 때 2의 곱셈들은 다 빼고 돌리면 됨.
+#2. 소수 판정 식에 소수 판정하는 데 중복되는 부분 있음
+#3. 4번 구간소수 인풋의 사용에 대한 설명 부족
+#4. 4번 숫자 두 개 입력하고 정의내리는 5줄을 가독성, 간결하게 해낼 수 있음.
+
 choice = int(input("문제를 골라주세요. \n 1) Fahrenheit -> Celsius 2) Celsius -> Fahrenheit 3) Prime1 4) Prime 2 5)QUIT\n : "))
 # 1번 : 화씨 -> 섭씨 변환
 if choice == 1:
@@ -42,13 +48,17 @@ elif choice == 4:
         is_prime = True
 
         if number < 2:
-            pass
+            continue #pass
         else:
-            for i in range(2, number):
+            i = 2
+            while i*i < number: # performance issue
                 if number % i == 0:
                     is_prime = False
                     break
-            if is_prime: print(number, end=' ')
+                i += i
+            if is_prime:
+                pass
+                print(number, end=' ')
     print()
 if choice == 5:
     print("Terminate Program")
