db0d37c7a34b38a4d658a434b319c950238ea5e4
vben <tjdcksl20@gmail.com>
2024-01-17 14:43:30 +0900

    v2.3 day04

--- day04.py
+++ day04.py
@@ -1,46 +1,18 @@
-# subjects = ['a','b','c']
-subjects = ['a',['b','c'],'d']
-# print(subjects[::-1])
-# subjects[::-1]
-# subjects.reverse() # 리버스 함수는 원본을 바꿈.
-# print(subjects)
+#8chapter "dict, set"
+sugang = dict(python = "kim", cpp = "sung", db = "kang")
+# print(sugang)
+# sugang["datastructure"] = 'kim' # add
+# print(sugang)
+# sugang["datastructure"] = 'park'    # update
+# print(sugang)
+# print(sugang['db'])     #[key]
+# print(sugang.get('db')) #get()
+# print(sugang.get('ooo', 'not exist')) #get (키, none 출력어 설정)
+for subject, professor in sugang.items():
+    print(f'{subject} 과목 담당교수는 {professor}입니다.')
 
-# #subjects.remove("java")
-# #subjects.sort(reverse=True)     # TypeError: '<' not supported between instances of 'int' and 'str'
-# subjects.sort(reverse=False) #desc => sort :
-# print(subjects)
-#
-# copy_subjects = sorted(subjects)
-# print(subjects)
-# print(copy_subjects)
-
-#7.2.23 복사하기
-import copy
-a = subjects
-b = subjects.copy()
-c = list(subjects)
-d = subjects[:]
-e = copy.deepcopy(a)
-print(subjects, a, b, c, d, e)     #원소 중 immutable한 값을 바꾸면 .copy()만 바뀜
-subjects[1][1] ="X"
-print(subjects, a, b, c, d, e)     #원소 중 mutable한 값을 바꾸면 전부 바뀐 상태로 적용.
-
-#list comprehension
-squares = list()
-# squares.append(1*1)
-# squares.append(2*2)
-# squares.append(3*3)
-# squares.append(4*4)
-# squares.append(5*5)
-# print(squares)
-
-# for i in range(1,6,1):
-#     squares.append(i*i)
-# print(squares)
-
-# squares = [i*i for i in range(1, 6, 1)]
-# print(squares)
-
-even_squares = [i*i for i in range(1, 6, 1) if i % 2 == 0]
-print(even_squares)
-
+for s in sugang.items():    #튜플 형식으로 받기.
+    print(s)
+print()
+for v in sugang.values():
+    print(v)
