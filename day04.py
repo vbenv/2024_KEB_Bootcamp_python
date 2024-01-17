f1b40a856414357633e9547d54f03d1b29069da1
vben <tjdcksl20@gmail.com>
2024-01-17 14:17:32 +0900

    v2.2 day04

--- day04.py
+++ day04.py
@@ -1,26 +1,46 @@
-# t1 = (5)
-# t2 = 5,
-# t3 = (5,)
-# t4 = (5, 7)
-# t5 = 5, 7
-# print(type(t1), type(t2), type(t3), type(t4), type(t5))
-# t6 = "python", "kim" # packing
-# print(type(t6), t6[1])
-# subject, prof = t6  # unpacking ; 개수가 맞아야 함.
-# #a, b, c = t6    #   ValueError: not enough values to unpack (expected 3, got 2)
-# print(prof)
-# print(subject)
-# t7 = ()
-# t8 = tuple()    #   기존 다른 자료 구조의 것을 튜플로 변환하려 할 때 많이 씀.
-# print(type(t7), type(t8),type(9), type((9,)))
-# t9 = 1, 2, 3
-# t10 = 1, 2,
-# print(t9 == t10)
-# print(t9 > t10)
-# t11 = 4.43,
-# t12 = 3.97, 4.1, 3.27
-# print(t11 + t12)
-# print(id(t11))
-# t11 += t12  # t11 = t11 + t12
-# print(t11)
-# print(id(t11))  # 주소가 바뀜.
+# subjects = ['a','b','c']
+subjects = ['a',['b','c'],'d']
+# print(subjects[::-1])
+# subjects[::-1]
+# subjects.reverse() # 리버스 함수는 원본을 바꿈.
+# print(subjects)
+
+# #subjects.remove("java")
+# #subjects.sort(reverse=True)     # TypeError: '<' not supported between instances of 'int' and 'str'
+# subjects.sort(reverse=False) #desc => sort :
+# print(subjects)
+#
+# copy_subjects = sorted(subjects)
+# print(subjects)
+# print(copy_subjects)
+
+#7.2.23 복사하기
+import copy
+a = subjects
+b = subjects.copy()
+c = list(subjects)
+d = subjects[:]
+e = copy.deepcopy(a)
+print(subjects, a, b, c, d, e)     #원소 중 immutable한 값을 바꾸면 .copy()만 바뀜
+subjects[1][1] ="X"
+print(subjects, a, b, c, d, e)     #원소 중 mutable한 값을 바꾸면 전부 바뀐 상태로 적용.
+
+#list comprehension
+squares = list()
+# squares.append(1*1)
+# squares.append(2*2)
+# squares.append(3*3)
+# squares.append(4*4)
+# squares.append(5*5)
+# print(squares)
+
+# for i in range(1,6,1):
+#     squares.append(i*i)
+# print(squares)
+
+# squares = [i*i for i in range(1, 6, 1)]
+# print(squares)
+
+even_squares = [i*i for i in range(1, 6, 1) if i % 2 == 0]
+print(even_squares)
+
