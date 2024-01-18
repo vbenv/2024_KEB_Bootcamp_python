ba7bfb91adad5bedc2a9dad1327849525f37e64b
vben <tjdcksl20@gmail.com>
2024-01-18 10:52:15 +0900

    v2.9 prime number function day05

--- day05.py
+++ day05.py
@@ -1,19 +1,32 @@
-#empty_set 을 만드는 유일한 방법 = set()
-#set 으로 list, tuple, dict만들 수 있으나 dict의 경우 key값만 불러옴
-#set과 dict은 sequence(순서)가 없음 -> 순서가 중요하지 않을 때 사용
-#set은 중복 허용X
-# & : set intersection 구할 때 사용. = 변수명.interseciont(다른 변수명)
-#| = union() : 합집합.
-# - = difference() : 차집합.
-# ^ = symmetric_difference() : 대칭 차집합.(두 셋 모두에 포함되지 않는 항목)
-#<= = issubset() : 첫번째 셋이 두 번째 셋의 부분집합인지 확인
+#Chapter 9. Function
+# def : 함수가 하는 일 1. 매개변수 정의하기     2. 결과 호출하기
+# 함수 이름 정할 때 가독성이 좋게 짜자. 결과값이 어떻게 나올지 예측 가능한 이름!
+#함수는 독립적인 코드.
 
-#set 컴프리헨션
-#a_set = {number for number in rage(1,6) if number % 3 == 1} #
-#immutable set이 존재. (본래는 mutable) => frozenset() ;읽기 전용
-yahoo = {
-'wahoo' : {'a','b'},
-'wohoo' : {'w','h'},
-'iohoo' : {'w','a'}
-}
-print(not yahoo['wahoo'] & {'h'})
+def isprime(n) -> bool:     #함수 다음 -> 'type' : 타입명을 명시해줄 수 있음. 없어도 잘 돌아감.
+    """
+    매개변수로 넘겨 받은 수가 소수인지 여부를 boolean으로 리턴
+    :param n: 판정할 매개변수
+    :return: 소수면 True, 소수가 아니면 False
+    """     #함수가 어떤 매뉴얼이 있는지 document를 달아두는거다. => help(isprime): 로 확인 가능
+    if n < 2:
+        return False
+    else:
+        i = 2
+        while i*i <= n:
+            if n % i == 0:
+                return False
+            i += 1
+        return True
+
+
+numbers = input("Input first second number : ").split()
+n1 = int(numbers[0])
+n2 = int(numbers[1])
+
+if n1 > n2:
+    n1, n2 = n2, n1
+
+for number in range(n1, n2+1):
+    if isprime(number):     #prime이 맞는지 확인하기 위한 함수.
+        print(number, end=' ')
