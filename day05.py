677f06ab40b73efee9655aa03850081ab547d50a
vben <tjdcksl20@gmail.com>
2024-01-18 10:06:17 +0900

    v2.8 day05

--- day05.py
+++ day05.py
@@ -1,16 +1,19 @@
-#8.10 연습문제 복습
+#empty_set 을 만드는 유일한 방법 = set()
+#set 으로 list, tuple, dict만들 수 있으나 dict의 경우 key값만 불러옴
+#set과 dict은 sequence(순서)가 없음 -> 순서가 중요하지 않을 때 사용
+#set은 중복 허용X
+# & : set intersection 구할 때 사용. = 변수명.interseciont(다른 변수명)
+#| = union() : 합집합.
+# - = difference() : 차집합.
+# ^ = symmetric_difference() : 대칭 차집합.(두 셋 모두에 포함되지 않는 항목)
+#<= = issubset() : 첫번째 셋이 두 번째 셋의 부분집합인지 확인
 
-squares = {n: pow(n, 2) for n in range(10)}
-#squares = {n: n**2 for n in range(10)}
-#squares = {n: n*n for n in range(10)}
-print(squares)
-
-# univ = 'inha university'
-# counts_alphabet = {letter: univ.count(letter) for letter in univ}   #dictionary comprehension
-# print(counts_alphabet)
-
-# univ = 'inha university'
-# counts_alphabet = dict()
-# for letter in univ:
-#     counts_alphabet[letter] = univ.count(letter)
-# print(counts_alphabet)
+#set 컴프리헨션
+#a_set = {number for number in rage(1,6) if number % 3 == 1} #
+#immutable set이 존재. (본래는 mutable) => frozenset() ;읽기 전용
+yahoo = {
+'wahoo' : {'a','b'},
+'wohoo' : {'w','h'},
+'iohoo' : {'w','a'}
+}
+print(not yahoo['wahoo'] & {'h'})
