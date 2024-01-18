b4fa38e90aa5fe01f007d691022b5aa2856635a7
vben <tjdcksl20@gmail.com>
2024-01-18 13:23:35 +0900

    3.1 day05

--- day05.py
+++ day05.py
@@ -1,48 +1,12 @@
-# prime number with function
-def isprime(n) -> bool:
-    """
-       매개변수로 넘겨 받은 수가 소수인지 여부를 boolean으로 리턴
-       :param n: 판정할 매개변수
-       :return: 소수면 True, 소수가 아니면 False
-       """
-    if n < 2:
-        return False
-    else:
-        i = 2
-        while i * i <= n:
-            if n % i == 0:
-                return False
-            i += 1
-        return True
-while True:
-     menu = input("1) Fahrenheit -> Celsius   2) Celsius -> Fahrenheit   3) Prime1   4) Prime2   5) Quit program : ")
+#매개변수, 인수
+#none <-> False 구분.
+#위치 인수 / 키워드 / 기본 매개변수; 바꿀 수 있음
+def a(n1,n2):
+    print(n1,n2)
+def a(n):
+    print(n)
 
-     if menu == '1':
-         fahrenheit = float(input('Input Fahrenheit : '))
-         print(f'Fahrenheit : {fahrenheit}F, Celsius : {((fahrenheit-32.0)*5.0/9.0):.4f}C')
-     elif menu == '2':
-         celsius = float(input('Input Celsius : '))
-         print(f'Celsius : {celsius}C, Fahrenheit : {((celsius*9.0/5.0)+32.0):.4f}F')
-     elif menu == '3':
-         number = int(input("Input number : "))
-         if isprime(number):
-             print(f'{number} is prime number')
-         else:
-             print(f'{number} is NOT prime number!')
-     elif menu == '4':
-         numbers = input("Input first second number : ").split()
-         n1 = int(numbers[0])
-         n2 = int(numbers[1])
+a(7)
+a(7,11)
+# 자바 or c에서는 두 개 다 오버라이딩 되어 출력되나 파이썬에서는 지원하지 않음.
 
-         if n1 > n2:
-             n1, n2 = n2, n1
-
-         for number in range(n1, n2 + 1):
-             if isprime(number):
-                 print(number, end=' ')
-         print()
-     elif menu == '5':
-         print('Terminate Program.')
-         break
-     else:
-         print('Invalid Menu!')
