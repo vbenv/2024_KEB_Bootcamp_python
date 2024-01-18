ba63a06f118ab9c8b8eed9d824337dbd2434bbff
vben <tjdcksl20@gmail.com>
2024-01-18 11:05:28 +0900

    v3.0 prime number with function day 05

--- day05.py
+++ day05.py
@@ -1,32 +1,48 @@
-#Chapter 9. Function
-# def : 함수가 하는 일 1. 매개변수 정의하기     2. 결과 호출하기
-# 함수 이름 정할 때 가독성이 좋게 짜자. 결과값이 어떻게 나올지 예측 가능한 이름!
-#함수는 독립적인 코드.
-
-def isprime(n) -> bool:     #함수 다음 -> 'type' : 타입명을 명시해줄 수 있음. 없어도 잘 돌아감.
+# prime number with function
+def isprime(n) -> bool:
     """
-    매개변수로 넘겨 받은 수가 소수인지 여부를 boolean으로 리턴
-    :param n: 판정할 매개변수
-    :return: 소수면 True, 소수가 아니면 False
-    """     #함수가 어떤 매뉴얼이 있는지 document를 달아두는거다. => help(isprime): 로 확인 가능
+       매개변수로 넘겨 받은 수가 소수인지 여부를 boolean으로 리턴
+       :param n: 판정할 매개변수
+       :return: 소수면 True, 소수가 아니면 False
+       """
     if n < 2:
         return False
     else:
         i = 2
-        while i*i <= n:
+        while i * i <= n:
             if n % i == 0:
                 return False
             i += 1
         return True
+while True:
+     menu = input("1) Fahrenheit -> Celsius   2) Celsius -> Fahrenheit   3) Prime1   4) Prime2   5) Quit program : ")
 
+     if menu == '1':
+         fahrenheit = float(input('Input Fahrenheit : '))
+         print(f'Fahrenheit : {fahrenheit}F, Celsius : {((fahrenheit-32.0)*5.0/9.0):.4f}C')
+     elif menu == '2':
+         celsius = float(input('Input Celsius : '))
+         print(f'Celsius : {celsius}C, Fahrenheit : {((celsius*9.0/5.0)+32.0):.4f}F')
+     elif menu == '3':
+         number = int(input("Input number : "))
+         if isprime(number):
+             print(f'{number} is prime number')
+         else:
+             print(f'{number} is NOT prime number!')
+     elif menu == '4':
+         numbers = input("Input first second number : ").split()
+         n1 = int(numbers[0])
+         n2 = int(numbers[1])
 
-numbers = input("Input first second number : ").split()
-n1 = int(numbers[0])
-n2 = int(numbers[1])
+         if n1 > n2:
+             n1, n2 = n2, n1
 
-if n1 > n2:
-    n1, n2 = n2, n1
-
-for number in range(n1, n2+1):
-    if isprime(number):     #prime이 맞는지 확인하기 위한 함수.
-        print(number, end=' ')
+         for number in range(n1, n2 + 1):
+             if isprime(number):
+                 print(number, end=' ')
+         print()
+     elif menu == '5':
+         print('Terminate Program.')
+         break
+     else:
+         print('Invalid Menu!')
