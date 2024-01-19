8a2670e5c950cc8e355ab65d130912fcb0780860
vben <tjdcksl20@gmail.com>
2024-01-19 10:54:48 +0900

    v3.9 day06

--- day06.py
+++ day06.py
@@ -1,50 +1,30 @@
-#연습문제
-#9.1
-# def good() -> list:
-#     harry_porter = input().split()
-#     return harry_porter
-#
-# print(good())
+import random
 
-#9.2
-# def get_odds(n) -> int:
-#     '''
-#     1부터 n까지의 홀수를 발생시키는 제너레이터
-#     :param n: int
-#     :return: int
-#     '''
-#     for i in range(1, n+1, 2):
-#         yield i
-#
-# cnt = 0
-# odds = get_odds(10)
-# for odd in odds:
-#     cnt += 1
-#     if cnt == 3:
-#         print(f'Third number is {odd}')
-#         break
+# numbers = list()
+# for i in range(5):
+#     numbers.append(random.randint(1,100))
+# print(numbers)
 
-#9.3 open closed principal [OCP]
-def test(f):
-    """
-    데코레이터 함수, 함수 시작하면 start 출력, 끝나면 end 출력
-    :param f: function
-    :return: closure function
-    """
-    #def test_in(*args, **kwargs):
-    def test_in():
-        print('start')
-        #result = f(*args, **kwargs)
-        f()
-        print('end')
-        #return result
-    return test_in  #closure라서 return
+numbers = [random.randint(1,100) for i in range(5)]
+print(numbers)
 
-@test
-def greeting():
-    print("안녕하세요~")
-
-greeting()
-
-# t = test(greeting)    #@test 안 쓸 때 이렇게 호출.
-# t()
+try:
+    pick = int(input(f"Input index (0 ~ {len(numbers)-1}) : "))
+    print(numbers[pick])
+    print(5/0)
+except ValueError as err:
+    print(f"Input Only Number~\n{err}")
+except IndexError as err:
+    print(f"Out of range : Wrong index number\n{err}")
+except ZeroDivisionError as err:
+    try:
+        pick = int(input(f"Input index (0 ~ {len(numbers) - 1}) : "))
+        print(numbers[pick])
+    except ValueError as err:
+        print(f"Input Only Number~\n{err}")
+    except IndexError as err:
+        print(f"Out of range : Wrong index number\n{err}")
+    except Exception:
+        print(f"The denominator cannot be 0\n{err}")
+except Exception:
+    print("Error occurs")
