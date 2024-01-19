4f0f89e028f8bfc28406f154653f67ace0c8086b
vben <tjdcksl20@gmail.com>
2024-01-19 11:23:42 +0900

    v4.1 decorator

--- day06.py
+++ day06.py
@@ -1,29 +1,14 @@
-import random
+def desc(f):
+	def wrapper():
+		print("w")
+		f()
+	#print("a")
+	return wrapper
 
+# @desc
+def something():
+	print("do sth")
 
-class OopsException(Exception):
-    pass
-
-# numbers = list()
-# for i in range(5):
-#     numbers.append(random.randint(1, 100))
-numbers = [random.randint(1, 100) for i in range(10)]
-print(numbers)
-
-try:
-    pick = int(input(f"Input index (0 ~ {len(numbers)-1}) : "))
-    print(numbers[pick])
-    print(5/2)
-    raise OopsException("Oops~~")  # exception!
-except IndexError as err:
-    print(f"Wrong index number\n{err}")
-except ValueError as err:
-    print(f"Input only number~\n{err}")
-except ZeroDivisionError as err:
-    print(f"The denominator cannot be 0.\n{err}")
-# except OopsException as err:
-#     print(f"Oops Oops {err}")
-except Exception as err:
-    print(f"Error occurs : {err}")
-else:
-    print(f"Program terminate")
+s = desc(something)
+s()
+#something()
