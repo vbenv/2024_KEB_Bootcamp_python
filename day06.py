1f35c915db0de8fab7fd9d4a709ce44da8536e16
vben <tjdcksl20@gmail.com>
2024-01-19 14:43:14 +0900

    v4.3 day06

--- day06.py
+++ day06.py
@@ -1,23 +1,33 @@
-# class Pokemon():
 class Pokemon:
-	def __init__(self, name): # self는 기본값. 그 뒤에 변수
+	def __init__(self, name):
 		self.name = name
-		print(f"{name} 포켓몬스터 생성")
 
 	def attack(self, target):
-		print(f'{self.name}이(가) {target.name}을(를) 공격!')
+		print(f"{self.name}이(가) {target.name}을(를) 공격!")
 
-charizard = Pokemon("리자몽")
-pikachu = Pokemon("피카츄")
-squirtle = Pokemon("꼬부기")
-print(pikachu.name)
-charizard.attack(squirtle)
-#해당 객체마다 단 한번만 태어날 수 있음.
-#이 프로그램이 끝나면 객체는 소멸됨
-# pikachu.name = ("피카츄")
-# pikachu.nemesis = squirtle
-# print(pikachu.name)
-# print(pikachu.nemesis) #객체를 어떻게 출력할지 안정해줌
-# pikachu.nemesis.name = "꼬부기"#squirtle.name = ("꼬부기")
-# print(squirtle.name)
-#
+
+class Pikachu(Pokemon):		#자식클래스이름(부모클래스이름)
+	def __init__(self, name, type):
+		super().__init__(name)
+		self.type = type
+	def attack(self, target):
+		print(f"{self.name}이(가) {target.name}을(를) {self.type}공격!")
+
+	def electric_info(self):
+		print("전기 계열의 공격")
+
+class Squirtle(Pokemon):		#자식클래스이름(부모클래스이름)
+	pass
+
+p1 = Pikachu("피카츄","전기") 	#("이름", "타입")
+p2 = Squirtle("꼬부기")
+p3 = Pokemon("아무개")
+p1.electric_info()
+#p3.electric_info()
+p1.attack(p2)
+p2.attack(p1)
+
+print(p1.name, p1.type)
+print(issubclass(Pikachu, Pokemon))		#상속 구조를 확인해볼 때 사용
+
+
