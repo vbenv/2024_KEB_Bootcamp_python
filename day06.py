d197e7ebe7e5b33ce68ee2152c6c5e0cc5bd9a0e
vben <tjdcksl20@gmail.com>
2024-01-19 16:39:02 +0900

    v4.8 day06

--- day06.py
+++ day06.py
@@ -1,24 +1,24 @@
 class FlyingMixin:
     def fly(self):
-        return f"{self.hidden_name}이(가) 하늘을 훨훨 날아갑니다~"
+        return f"{self.__name}이(가) 하늘을 훨훨 날아갑니다~"
 
 class SwimmingMixin:
     def swim(self):
-        return f"{self.hidden_name}이(가) 수영을 합니다."
+        return f"{self.__name}이(가) 수영을 합니다."
 
 class Pokemon:
     def __init__(self, name):
-        self.hidden_name = name
+        self.__name = name
 
     def attack(self):
         print("공격~")
 
     @property
     def name(self):
-        return self.hidden_name
+        return self.__name
     @name.setter
     def name(self, new_name):
-        self.hidden_name = new_name
+        self.__name = new_name
 
    # name = property(get_name, set_name)
 
@@ -36,7 +36,10 @@ c1 = Charizard("리자몽")
 # g1.set_name("잉어킹")
 # print(g1.get_name())
 
-# property 2nd
+# property 3nd
 print(g1.name)
-g1.name = "잉어킹"
-print(g1.name)
+# print(g1.__name) #direct는 안되고 프로포티로만 가능.
+print(g1._Pokemon__name) #직접 접근이 아니라 우회해서 접근.
+g1.__name = "잉어킹" # 안된다
+g1._Pokemon__name = "잉어킹" #객체.클래스명__함수 로 접근 가능하다.
+print(g1._Pokemon__name) #사실상 private개념은 없는 걸로
