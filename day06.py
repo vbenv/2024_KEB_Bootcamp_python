43f5dfe8481f03fdadd755efbea1d132c129347b
vben <tjdcksl20@gmail.com>
2024-01-19 10:58:30 +0900

    v3.81 day 06

--- day06.py
+++ day06.py
@@ -1,30 +1,28 @@
-import random
+def factorial_repetition(n) -> int:
+    '''
+    반복문을 이용한 팩토리얼 함수
+    :param n: 정수, int
+    :return: 팩토리얼 값, int
+    '''
+    result = 1
+    for i in range(2, n + 1):
+        result = result * i
+    return result
 
-# numbers = list()
-# for i in range(5):
-#     numbers.append(random.randint(1,100))
-# print(numbers)
 
-numbers = [random.randint(1,100) for i in range(5)]
-print(numbers)
+def factorial_recursion(n):
+    '''
+    재귀함수를 사용한 팩토리얼 함수
+    :param n: 정수, int
+    :return: function, int
+    '''
+    if n == 1:
+        return n
+    else:
+        return n * factorial_recursion(n - 1)
 
-try:
-    pick = int(input(f"Input index (0 ~ {len(numbers)-1}) : "))
-    print(numbers[pick])
-    print(5/0)
-except ValueError as err:
-    print(f"Input Only Number~\n{err}")
-except IndexError as err:
-    print(f"Out of range : Wrong index number\n{err}")
-except ZeroDivisionError as err:
-    try:
-        pick = int(input(f"Input index (0 ~ {len(numbers) - 1}) : "))
-        print(numbers[pick])
-    except ValueError as err:
-        print(f"Input Only Number~\n{err}")
-    except IndexError as err:
-        print(f"Out of range : Wrong index number\n{err}")
-    except Exception:
-        print(f"The denominator cannot be 0\n{err}")
-except Exception:
-    print("Error occurs")
+
+number = int(input("number : "))
+print(factorial_repetition(number))
+print(factorial_recursion(number))
+print(globals())
