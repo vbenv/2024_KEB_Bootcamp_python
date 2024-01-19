c149e196ca38efe66f4f1793ab8e425e73d67768
vben <tjdcksl20@gmail.com>
2024-01-19 15:21:26 +0900

    v4.5 day06

--- day06.py
+++ day06.py
@@ -10,6 +10,14 @@ class Pokemon:
 	def __init__(self, name):
 		self.name = name
 
+	def attack(self):
+		print("공격")
+
+	def get_name(self):
+		return self.name
+
+	def set_name(self, new_name):
+		self.name = new_name
 class Charizard(Pokemon, FlyingMixin):
 	pass
 
@@ -20,36 +28,10 @@ g1 = Gyarados("갸라도스")
 c1 = Charizard("리자몽")
 print(c1.fly())		#하늘을 나는 것만 가져다 씀.
 print(g1.swim())
+c1.attack()
+Charizard.attack(c1)	#클래스명.함수(객체)
 
+print(g1.get_name())
+g1.set_name("잉어킹")
+print(g1.name)
 
-
-
-
-# class Animal:
-# 	def says(self):
-# 		return 'I speak!'
-#
-# class Horse(Animal):
-# 	def says(self):
-# 		return "heee"
-#
-# class Donkey(Animal):
-# 	def says(self):
-# 		return "HOooo"
-#
-# class Mule(Donkey, Horse):
-# 	#def says(self):
-# 	#return '노새 노새 젊어서 노새~'
-# 	pass
-#
-# class Hinny(Horse, Donkey):
-# 	#def says(self):
-# 		#return '버새 버새'
-# 	pass
-# # mule에 있는 says 없으면->Donkey -> Horse->그의 부모인 animal
-# m1 = Mule()
-# m2 = Hinny()
-# print(m1.says())
-# print(m2.says())
-#
-# print(Hinny.__mro__)
