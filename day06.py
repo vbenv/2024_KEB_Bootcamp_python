b57ae3c8a97b8713ced1832ee91fa054c3462a24
vben <tjdcksl20@gmail.com>
2024-01-22 10:16:38 +0900

    v5.0 day07

--- day06.py
+++ day06.py
@@ -1,36 +1,68 @@
-# # 10.1 연습문제
-# class Thing():
-#     pass
-#
-# example = Thing()
-# print(Thing == example)
-# print(Thing)
-# print(example)
-# 결론 : 클래스 띵 / 띵의 객체
+#포켓몬 게임 만들기
+import random
 
-# 10.2
-class Thing2():
-    letter='abc'
+user_name = input("당신의 닉네임을 적어주세요 : ")
+print(f"안녕하세요 {user_name}님 포켓몬 세상에 오신 것을 환영합니다.")
+# class Character():
+#     def __init__(self,name):
+#         self.name = name
+#         return (f"안녕하세요 {name}님 포켓몬 세상에 오신 것을 환영합니다.")
+player = input("당신의 캐릭터를 1~3번 중에 골라주세요.")
+if player == '1':
+    player = "루피"
+    print("당신은 루피를 고르셨습니다.")
+if player == '2':
+    player = "나미"
+    print("당신은 나미를 고르셨습니다.")
+if player == '3':
+    player = "우솝"
+    print("당신은 우솝을 고르셨습니다.")
 
-print(Thing2.letter)
+class Player():
 
-# 10.3
-class Thing3():
-    letter = 'xyz'
 
-print(Thing3.letter)
-#정답은 NO!
+class FlyingMixin:
+    def fly(self):
+        return f"{self.__name}이(가) 하늘을 훨훨 날아갑니다~"
 
-# 10.4
-class Element():
-        def __init__(self, name, symbol, number):
-            self.name = name
-            self.symbol = symbol
-            self.number = number
-#hydrogen = Element('Hydrogen', 'H', 1)
-#
-# 10.5
-el_dict ={'name':'Hydrogen','symbol':'H','number':1}
-hydrogen = Element(el_dict['name'],['symbol'],['number'])
-print(hydrogen.name)
-# 10.6
+class SwimmingMixin:
+    def swim(self):
+        return f"{self.__name}이(가) 수영을 합니다."
+
+class Pokemon:
+    def __init__(self, name):
+        self.__name = name
+
+    def attack(self):
+        print("공격~")
+
+    @property
+    def name(self):
+        return self.__name
+    @name.setter
+    def name(self, new_name):
+        self.__name = new_name
+
+   # name = property(get_name, set_name)
+
+
+class Charizard(Pokemon, FlyingMixin):
+    pass
+
+class Gyarados(Pokemon, SwimmingMixin):
+    pass
+
+g1 = Gyarados("갸라도스")
+c1 = Charizard("리자몽")
+
+# print(g1.get_name())
+# g1.set_name("잉어킹")
+# print(g1.get_name())
+
+# property 3nd
+#print(g1.name)
+# print(g1.__name) #direct는 안되고 프로포티로만 가능.
+#print(g1._Pokemon__name) #직접 접근이 아니라 우회해서 접근.
+#g1.__name = "잉어킹" # 안된다
+#g1._Pokemon__name = "잉어킹" #객체.클래스명__함수 로 접근 가능하다.
+#print(g1._Pokemon__name) #사실상 private개념은 없는 걸로
---
+++ dya07.py
@@ -0,0 +1,40 @@
+class FlyingMixin:
+    def fly(self):
+        return f"{self.__name}이(가) 하늘을 훨훨 날아갑니다~"
+
+class SwimmingMixin:
+    def swim(self):
+        return f"{self.__name}이(가) 수영을 합니다."
+
+class Pokemon:
+    def __init__(self, name):
+        self.__name = name
+
+    def attack(self):
+        print("공격~")
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, new_name):
+        self.__name = new_name
+
+    #name = property(get_name, set_name)
+    def __str__(self):  #매직메서드
+        return self.__name + "입니다."
+    def __add__(self, other):
+        return self
+class Charizard(Pokemon, FlyingMixin):
+    pass
+
+class Gyarados(Pokemon, SwimmingMixin):
+    pass
+
+g1 = Gyarados("갸라도스")
+c1 = Charizard("리자몽")
+
+print(g1)
+print(c1)
+print
