b2993a6ada8e3984b921719d9c2b633225eedfa0
vben <tjdcksl20@gmail.com>
2024-01-22 11:33:03 +0900

    v5.3 day 07

--- dya07.py
+++ dya07.py
@@ -17,51 +17,12 @@ class FlyWithWings(FlyingBehavior):
     def fly(self):
         return f"날개로 하늘을 훨훨 날아갑니다"
 
-
-class SwimmingBehavior:
-    def swim(self):
-        return f"{self.__name}이(가) 수영을 합니다."
-
-class Pokemon:
+class Pikachu:
     def __init__(self, name, hp, fly):
-        self.__name = name
+        self.name = name
         self.hp = hp
-        self.fly_behavior = fly  # aggregation (has-a)
-
-
-    def attack(self):
-        print("공격~")
-
-    @property
-    def name(self):
-        return self.__name
-
-    @name.setter
-    def name(self, new_name):
-        self.__name = new_name
-
-    # magic method
-    def __str__(self):
-        return self.__name + " 입니다"
-
-    def __add__(self, target):
-        #return self.__name + " + " + target.__name
-        return f"두 포켓몬스터 체력의 합은 {self.hp + target.hp}입니다."
-
-
-class Charizard(Pokemon):
-    pass
-
-class Pikachu(Pokemon):
-    pass
+        self.fly_behavior = fly # flybehavior 클래스를 받는 것, aggregation
 
 nofly = NoFly()
-g1 = Pikachu("피카츄", 35, nofly)  # LSP
-wings = FlyWithWings()
-c1 = Charizard("리자몽", 120, wings)  # LSP
-print(c1.fly_behavior.fly())
-print(g1.fly_behavior.fly())
-print(g1)
-print(c1)
-print(g1+c1)
-#print(g1+200)
+p1 = Pikachu("피카츄", 35, nofly)
+print(p1.fly_behavior.fly())
